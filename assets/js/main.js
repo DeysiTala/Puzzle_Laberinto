8a55028d874cfbf905a4f9d5ef704804b06b4813
Deysi <sude.90@gmail.com>
2017-08-07 17:28:49 -0500

    activando funciones a los botones

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,3 +41,33 @@ for(var i =0; i<mapa.length; i++) {
 			     tablero.appendChild(tabla);
 	}
 
+/*var x =_ 0;
+var y = 0;*/
+
+/*var n = 0; 
+function cambiarPosition()
+{
+var el_div = document.getElementById('flecha');
+
+if(n == 0)
+{
+el_div.style.position = 'absolute';
+n = 1;
+}
+else
+{
+el_div.style.position = 'relative';
+n = 0;
+}
+} */
+function moverArriba() {
+tablero.style.top = tablero.offsetTop - 10;
+}
+
+
+function moverDerecha() {
+tablero.style.left = tablero.offsetLeft + 10;
+}
+function moverIzquierda() {
+tablero.style.left = tablero.offsetLeft - 10;
+}
